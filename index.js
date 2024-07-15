5d228896453d8f2b898f0d25186639957f98aec0
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2024-07-15 21:26:11 +0200

    Added functionality to the notification

---
+++ index.js
@@ -0,0 +1,28 @@
+/**popup notification */
+document.getElementById("allow-btn").addEventListener("click", function () {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        alert("Location access granted.");
+        console.log("Latitude: " + position.coords.latitude);
+        console.log("Longitude: " + position.coords.longitude);
+      },
+      function (error) {
+        alert("Location access denied.");
+      }
+    );
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+  closePopup();
+});
+
+document.getElementById("deny-btn").addEventListener("click", function () {
+  alert("Location access denied.");
+  closePopup();
+});
+
+function closePopup() {
+  document.getElementById("popup").style.visibility = "hidden";
+  document.getElementById("popup").style.opacity = "0";
+}
