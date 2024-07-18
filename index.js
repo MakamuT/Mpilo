661ed0f67e792f79e30e90f674666b206a93b50b
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2024-07-18 15:26:08 +0200

    Added firebase sdk and added script tag in register.html

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-/**popup notification */
-document.getElementById("allow-btn").addEventListener("click", function () {
+/***document.getElementById("allow-btn").addEventListener("click", function () {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       function (position) {
@@ -25,4 +24,26 @@ document.getElementById("deny-btn").addEventListener("click", function () {
 function closePopup() {
   document.getElementById("popup").style.visibility = "hidden";
   document.getElementById("popup").style.opacity = "0";
-}
+}  */
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCdYM08AxUTdm9fwipJFjvrfSHMNwgNOCQ",
+  authDomain: "mpilo-429010.firebaseapp.com",
+  databaseURL:
+    "https://mpilo-429010-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "mpilo-429010",
+  storageBucket: "mpilo-429010.appspot.com",
+  messagingSenderId: "132338768622",
+  appId: "1:132338768622:web:833d4c9237674e59a131ff",
+  measurementId: "G-VR7LQTQZJ4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
