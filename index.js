aefc3c4052e8ed1ed2ea0e49785a4a979281c831
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2024-07-18 20:17:29 +0200

    Added function to show message

--- index.js
+++ index.js
@@ -54,6 +54,15 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+function showMessage(message, divId) {
+  var messageDiv = document.getElementById(divId);
+  messageDiv.style.display = "block";
+  messageDiv.innerHTML = message;
+  messageDiv.style.opacity = 1;
+  setTimeout(function () {
+    messageDiv.style.opacity = 0;
+  }, 5000);
+}
 const signUp = document.getElementById("submitSignUp");
 signUp.addEventListener("click", (event) => {
   event.preventDefault();
@@ -66,13 +75,29 @@ signUp.addEventListener("click", (event) => {
   const auth = getAuth();
   const db = getFirestore();
 
-  createUserWithEmailAndPassword(auth, email, password).then(
-    (userCredential) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
       const user = userCredential.user;
       const userData = {
         email: email,
         fullName: fullName,
       };
-    }
-  );
+      showMessage("Account Created Successfully", "signUpMessage");
+      const docRef = doc(db, "users", user.uid);
+      setDoc(docRef, userData)
+        .then(() => {
+          window.location.href = "index.html";
+        })
+        .catch((error) => {
+          console.error("error writing document", error);
+        });
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      if (errorCode == "auth/email-already-in-use") {
+        showMessage("Email Address Already Exists !!!", "signUpMessage");
+      } else {
+        showMessage("unable to create User", "signUpMessage");
+      }
+    });
 });
