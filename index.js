fbbcf0818397d9c4166275354bc556ef781eb7cc
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2024-07-18 17:13:25 +0200

    import firebase configuration

--- index.js
+++ index.js
@@ -28,11 +28,17 @@ function closePopup() {
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getAuth,
+  createUserEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+import {
+  getFirestore,
+  setDoc,
+  doc,
+} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCdYM08AxUTdm9fwipJFjvrfSHMNwgNOCQ",
   authDomain: "mpilo-429010.firebaseapp.com",
