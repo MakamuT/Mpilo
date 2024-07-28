0f8d03b62a21371687d474372deaea020ca3196e
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2024-07-28 02:42:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
-/***document.getElementById("allow-btn").addEventListener("click", function () {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      function (position) {
-        alert("Location access granted.");
-        console.log("Latitude: " + position.coords.latitude);
-        console.log("Longitude: " + position.coords.longitude);
-      },
-      function (error) {
-        alert("Location access denied.");
-      }
-    );
-  } else {
-    alert("Geolocation is not supported by this browser.");
-  }
-  closePopup();
-});
-
-document.getElementById("deny-btn").addEventListener("click", function () {
-  alert("Location access denied.");
-  closePopup();
-});
-
-function closePopup() {
-  document.getElementById("popup").style.visibility = "hidden";
-  document.getElementById("popup").style.opacity = "0";
-}  */
-
-// SearchBar
-
-// API integration
-const API_KEY = "sk-proj-5UDmDED8RbyFxGwbX4CeT3BlbkFJWHsk6CWSjr3p0BHD7Brs";
+const OPENAI_API_KEY =
+  "sk-proj-GDBtvEfLQfUnui2x2ArsT3BlbkFJyVqli4JtveBRwmtrsjsH";
 const searchButton = document.querySelector("#search");
 
 async function getMessage() {
   console.log("clicked");
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer ${OPENAI_API_KEY}",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-4o",
+      messages: [
+        {
+          role: "system",
+          content: "You are a helpful assistant.",
+        },
+        {
+          role: "user",
+          content: "Hello!",
+        },
+      ],
+      max_tokens: 100,
+    }),
+  };
   try {
+    await fetch("https://api.openai.com/v1/chat/completions", options);
   } catch {}
 }
 
