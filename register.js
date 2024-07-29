bfd6b9d7c3c8de725cf86e4e6df15e2c6fee57ff
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2024-07-29 15:06:27 +0200

    Update register.js

--- register.js
+++ register.js
@@ -1,7 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +17,31 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// submit button
+const createAccount = document.getElementById("createAccount");
+createAccount.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  // inputs
+  const fullName = document.getElementById("fullName").value;
+  const emailLink = document.getElementById("emailLink").value;
+  const phoneNumber = document.getElementById("phoneNumber").value;
+  const username = document.getElementById("username").value;
+  const passwordLink = document.getElementById("passwordLink").value;
+
+  createUserWithEmailAndPassword(auth, emailLink, passwordLink)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      alert("Creating Account...");
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+      // ..
+    });
+});
