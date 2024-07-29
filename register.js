4669e90f8eeb743584f7d4912a642c6069a1696c
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2024-07-29 11:41:13 +0200

    Added firebase SDK

---
+++ register.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAQ8RRqmTJYB_clYp0VN8c4_02p29lwfZE",
+  authDomain: "mpilo-app-52481.firebaseapp.com",
+  projectId: "mpilo-app-52481",
+  storageBucket: "mpilo-app-52481.appspot.com",
+  messagingSenderId: "987304420031",
+  appId: "1:987304420031:web:5bf32361ae8d4c36c8dc0a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
